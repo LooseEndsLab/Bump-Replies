0bc6ed3f9c272e8447c0b0a1a2c2ab2c268d2616
Jdka1 <staryan.mehra@gmail.com>
2026-08-20 13:02:49 -0700

    send

---
+++ BumpReplies/BumpReplies/BumpRepliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumpRepliesApp.swift
+//  BumpReplies
+//
+//  Created by Aryan Mehra on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BumpRepliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
